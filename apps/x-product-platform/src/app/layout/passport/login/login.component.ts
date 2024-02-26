c837fd0eae1aead71a4f9af29ef33b371decb273
xu_xz <xuxiaozhong@cptgroup.cn>
2024-02-26 13:03:38 +0800

    feat: 登录页面

--- apps/x-product-platform/src/app/layout/passport/login/login.component.ts
+++ apps/x-product-platform/src/app/layout/passport/login/login.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'xp-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.less',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      UserCode: [null, [Validators.required]],
+      Password: [null, [Validators.required]],
+    });
+  }
+}
